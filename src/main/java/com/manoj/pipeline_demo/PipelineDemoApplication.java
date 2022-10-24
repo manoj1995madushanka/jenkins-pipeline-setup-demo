a24aad5e618f3ab0727b38a2806c9519207be710
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-10-24 12:19:44 +0530

    test continuous integration

--- src/main/java/com/manoj/pipeline_demo/PipelineDemoApplication.java
+++ src/main/java/com/manoj/pipeline_demo/PipelineDemoApplication.java
@@ -1,6 +1,8 @@
 package com.manoj.pipeline_demo;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -9,8 +11,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class PipelineDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PipelineDemoApplication.class, args);
-	}
+    private static Logger logger = LoggerFactory.getLogger(PipelineDemoApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("app started");
+        SpringApplication.run(PipelineDemoApplication.class, args);
+    }
 
 }
